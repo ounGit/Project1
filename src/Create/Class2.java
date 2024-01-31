3440abc0606f03fc0c2080a028168edb279cd285
oun run <oun.gihub001@gmail.com>
2024-01-31 08:01:12 +0700

    First Commit

---
+++ src/Create/Class2.java
@@ -0,0 +1,7 @@
+package Create;
+
+public class Class2 {
+	int sum ( int a, int b) {
+		return a + b;
+	}
+}
---
+++ src/Create/Main.java
@@ -0,0 +1,11 @@
+package Create;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello ProjectTest");
+		Class2 cls = new Class2();
+		System.out.println("The is value of A + B: " + cls.sum(9,1));
+	}
+
+}
